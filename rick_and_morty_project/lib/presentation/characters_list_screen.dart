e3b0e1d0628e55ce7980f22d60fdac18e72abf17
Chuijazy <chuijazy.2004@gmail.com>
2025-04-29 14:19:19 +0600

    init main

---
+++ rick_and_morty_project/lib/presentation/characters_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CharactersListScreen extends StatelessWidget {
+  const CharactersListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('characters list', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
